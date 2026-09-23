9688b850acd99559395e32ec86bae4b1b21b88ab
ChimaGascaJhonatan <jhonydav2004@gmail.com>
2026-09-23 16:02:28 -0500

    Delete domain directory

--- domain/repositories/promo_code_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../entities/promo_code.dart';
-
-abstract class PromoCodeRepository {
-  Stream<List<PromoCode>> getPromoCodes();
-  Future<void> addPromoCode(PromoCode promoCode);
-  Future<PromoCode?> validatePromoCode(String code);
-}
